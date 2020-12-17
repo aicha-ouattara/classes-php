2ed989aa3a55bc32a3d4cbe75d942cb331a921eb
chay <aicha.ouattara@laplateforme.io>
2020-12-17 23:30:50 +0100

    upd

--- user-pdo.php
+++ user-pdo.php
@@ -124,27 +124,27 @@ class User
 
     public function getAllInfos()
     {
-        return [$this->id, $this->login, $this->password, $this->email, $this->firstname , $this->lastname ];
+        return $this;
     }
 
     public function getLogin()
     {
-        return [$this->login];
+        return $this->login;
     }
 
     public function getEmail()
     {
-        return [$this->email];
+        return $this->email;
     }
 
     public function getFirstname()
     {
-        return [$this->firstname];
+        return $this->firstname;
     }
 
     public function getLastname()
     {
-        return [$this->lastname];
+        return $this->lastname;
     }
 
     public function refresh()
